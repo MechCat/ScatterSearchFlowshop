d0fa2c08774b37e06d254b62873ed046ae75a979
Uğur Sinan Eren <necrophagistan@gmail.com>
2021-04-27 08:27:44 +0300

    Random parameter generation and

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -70,17 +70,90 @@ export class HomeComponent implements OnInit {
 
   /** Runs the Scatter Search algorithm via *sss* service. */
   scatterSearch() {
-    this.wait = true;
-    const startTime = Date.now();
-    this.solution = this.sss.scatterSearch(this.ps);
+    { //#region  param gen (just generates, logs then terminates without solving)
+      // let grandparams = [];
+      // const params = [];
+      // params.push('iter');
+      // params.push('pop');
+      // params.push('ref %');
+      // params.push('good %');
+      // params.push('bad %');
+      // params.push('ref');
+      // params.push('good');
+      // params.push('bad');
+      // grandparams.push(params);
+
+      // for (let i = 0; i < 10; i++) {
+      //   const subparams = [];
+      //   const iterLimit = Utility.random(60, 20);
+      //   const popSize = Utility.random(200, 20);
+      //   const refRatio = Utility.random(100, 1);
+      //   const goodRefRatio = Utility.random(100);
+      //   const badRefRatio = 100 - goodRefRatio;
+      //   const refSize = Math.round(popSize * refRatio / 100);
+      //   const goodRefSize = Math.round(refSize * goodRefRatio / 100);
+      //   const badRefSize = Math.round(refSize * badRefRatio / 100);
+      //   subparams.push(iterLimit);
+      //   subparams.push(popSize);
+      //   subparams.push(refRatio);
+      //   subparams.push(goodRefRatio);
+      //   subparams.push(badRefRatio);
+      //   subparams.push(refSize);
+      //   subparams.push(goodRefSize);
+      //   subparams.push(badRefSize);
+      //   grandparams.push(subparams);
+      // }
+      // grandparams.forEach(x => {
+      //   x[2] = x[2] + '%';
+      //   x[3] = x[3] + '%';
+      //   x[4] = x[4] + '%';
+      // });
+      // console.log(grandparams);
+      // return;
+    } //#endregion
+
+    this.wait = !this.wait;
+    const data: any = [['Makespan', 'Runtime (ms)', 'Max Iteration', 'PopSize', 'RefRatio', 'Good Ref Ratio', 'Diverse Ref Ratio',
+      'RefSize', 'Good Ref Size', 'Diverse Ref Size', 'Sequence']];
+
+    const sampleNo = 3000;
+
+    for (let i = 0; i < sampleNo; i++) {
+      // Assign params
+      const iterLimit = Utility.random(60, 20);
+      const popSize = Utility.random(200, 20);
+      const refRatio = Utility.random(100, 1);
+      const goodRefRatio = Utility.random(100);
+      const badRefRatio = 100 - goodRefRatio;
+      const refSize = Math.round(popSize * refRatio / 100);
+      const goodRefSize = Math.round(refSize * goodRefRatio / 100);
+      const badRefSize = refSize - goodRefSize; // = Math.round(refSize * badRefRatio / 100);
+      console.log('params: iterLimit, popSize, refSize, goodRefSize, badRefSize:', iterLimit, popSize, refSize, goodRefSize, badRefSize);
+      // alg. diagnostics
+      this.sss.iterLimit = iterLimit;
+      this.sss.popSize = popSize;
+      this.sss.refSize.good = goodRefSize;
+      this.sss.refSize.diverse = badRefSize;
+
+      const startTime = Date.now();
+      // algorithm
+      this.solution = this.sss.scatterSearch(this.ps);
+      // fin & push into data
+      const rt = Date.now() - startTime;
+      console.log('>>>>>> curIt:', i, 'makespan: ', this.solution.makespan, 'runtime: ', rt + ' ms');
+      data.push([this.solution.makespan, rt, this.sss.iterLimit, popSize, refRatio, goodRefRatio, badRefRatio,
+        refSize, goodRefSize, badRefSize, this.solution.sequence]);
+    }
+    // visual stuff
     this.makespanDif.lbDifference = this.ps.problem.boundLower - this.solution.makespan;
     this.makespanDif.lbPercent = Math.round((this.makespanDif.lbDifference * 100 / this.ps.problem.boundLower) * 100) / 100;
     this.makespanDif.ubDifference = this.ps.problem.boundUpper - this.solution.makespan;
     this.makespanDif.ubPercent = Math.round((this.makespanDif.ubDifference * 100 / this.ps.problem.boundUpper) * 100) / 100;
     this.gantt.drawGanttChart(this.solution);
     this.fillTreeData(this.solution);
-    console.log('runtime: ', Date.now() - startTime + 'ms');
-    this.wait = false;
+    // output
+    Utility.cvsOutput(data, this.ps.problem.name + ' results');
+    this.wait = !this.wait;
   }
 
   /** Parses data from the selected problem text file. */
